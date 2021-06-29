cce4a5bebd93eb25294c0a5eb401b5b82cbd2919
Yasin Bhojani <74419103+yasinbhojani@users.noreply.github.com>
2021-06-29 19:20:38 +0530

    Update candy.cpp

--- candy.cpp
+++ candy.cpp
@@ -1,10 +1,3 @@
-// The candy machine sells candies, chips, gum, and cookies. You have been asked to write a program for this candy machine.
-
-// The program should do the following:
-// 1. Display products available,
-// 2. Display ordered products with Qty
-// 3. Product-wise bill.
-// 4. Total bill after buying all products.
 #include<iostream>
 #include<stdlib.h>
 #define candy_p 10
